d8a74b2029caa9e999bcafff558bc61687fc8c26
P1attos <111728034+P1attos@users.noreply.github.com>
2022-09-06 10:14:00 +0900

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+ 
+       Student Me = new Student();
+       Me.Surname = "Montales";
+       Me.firstName = "Kimberly Erika";
+       Me.middleInitial = 'N';
+       Me.dateOfBirth = 12/04/03;
+       Me.studentNumber = 2022104869;
+       Me.emailAddress = "montaleskn@students.national-u.edu.ph";
+       Me.iAmAwesome = true;
+       Me.sayMystudentNumber();
+       Me.sayMyEmailAddress();
+       Me.sayIamAwesome();
+    }
+ }
