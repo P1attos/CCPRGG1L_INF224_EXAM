f0c03ef449a849a11a64d197ba06604b656c9dad
P1attos <111728034+P1attos@users.noreply.github.com>
2022-10-11 11:49:01 +0800

    Update Exam.java

--- Exam2/Exam.java
+++ Exam2/Exam.java
@@ -5,16 +5,17 @@ public class Exam {
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
-        Pos();
-        lastDigits();
-        Names();
-        numSize();
-        alpha();
-        sNumber();
-        letterc();
-        course();
-        strand();
-        email();
+        Q1();
+        Q2();
+        Q3();
+        Q4();
+        Q5();
+        Q6();
+        Q7();
+        Q8();
+        Q9();
+        Q10();
+        Q11();
     }
 
         // Long exam 2
@@ -33,7 +34,7 @@ public class Exam {
         // print "ZERO" if the number is ZERO. 
         //(10 pts)
 
-        static void Pos(){
+        static void Q1(){
             System.out.println("1. Positive, negative, zero");
             System.out.print("Enter a number: ");
             int number = scan.nextInt();
@@ -61,7 +62,7 @@ public class Exam {
         // or a composite number.
         // (10 pts)
 
-        static void lastDigits(){
+        static void Q2(){
 
             System.out.println("2. PRIME or COMPOSITE number");
             System.out.print("Enter the last digit of your student number: ");
@@ -82,7 +83,7 @@ public class Exam {
         // 3. Create a method that will return the sum of the character length of your
         // firstname and your surname. (10 pts)
 
-        static void Names(){
+        static void Q3(){
             System.out.println("3. Length of your fullname");
             System.out.print("Enter your firstname: ");
             String firstName = scan.next();
@@ -97,7 +98,7 @@ public class Exam {
 
         // 4. Create a method that will take three whole numbers and print the largest number.
    
-        static void numSize(){
+        static void Q4(){
             System.out.println("4. Largest Number");
             System.out.print("Enter first number: ");
             int firstNumber = scan.nextInt();
@@ -119,7 +120,7 @@ public class Exam {
         // 5. Create a method that will print "VOWEL" if your name starts with
         // a vowel and "CONSONANT" if your name starts with a consonant.
         
-        static void alpha(){
+        static void Q5(){
 
             System.out.println("5. Vowel or Consonant");
             System.out.print("Enter the first character of your first name: ");
@@ -127,9 +128,9 @@ public class Exam {
     
             if (firstChar == 'a' || firstChar == 'e' || firstChar == 'i' || firstChar == 'o' || firstChar == 'u'
             || firstChar == 'A' || firstChar == 'E' || firstChar == 'I' || firstChar == 'O' || firstChar == 'U')
-                 System.out.println(true);
+                 System.out.println("VOWEL");
                  else
-                 System.out.println(false);
+                 System.out.println("CONSONANT");
     
             }
         
@@ -144,7 +145,7 @@ public class Exam {
         // 6. Create a condition to check if the entered student number is your student
         // number. (10 pts)
 
-        static void sNumber(){
+        static void Q6(){
             System.out.println("6. Student number validation");
         System.out.print("Enter your ten digit student number: ");
         int studentNumber = scan.nextInt();
@@ -162,7 +163,7 @@ public class Exam {
         // 7. Create a condition to check if your firstname is in UPPERCASE and your
         // lastname is in lowercase. (10 pts)
 
-        static void letterc(){
+        static void Q7(){
         System.out.println("7. Case sensitivity check");
         System.out.print("Enter your firstname in UPPERCASE: ");
         String firstName = scan.next();
@@ -188,37 +189,34 @@ public class Exam {
         // Condition 2
         // if the student is BSCS, ask again if the student is taking ML or DF.
 
-        static void course(){
-        System.out.println("8. Nested conditions");
-        System.out.print("Enter your enrolled course (BSIT or BSCS): ");
-        String course = scan.next();
-
-        // If BSIT
-        if (course.toLowerCase().equals("bsit") || course.toUpperCase().equals("BSIT")) {
-
-            System.out.print("Enter your specialization (MWAA or MAA): ");
-            String specialization1 = scan.next();
-            if (specialization1.toLowerCase().equals("mwaa") || specialization1.toUpperCase().equals("MWAA"))
-            {
-                System.out.println("YOUR SPEACILIZATION IS MWAA");
-            } else
-                System.out.println("YOUR SPEACILIZATION IS MAA");
-
+        static void Q8(){
+            System.out.println("8. Nested conditions");
+            System.out.print("Enter your enrolled course (BSIT or BSCS): ");
+            String course = scan.next();
+    
+           // if BSIT
+            if (course.toLowerCase().equals("bsit") || course.toUpperCase().equals("BSIT")) {
+                System.out.print("Enter your specialization (MWA or MAA): ");
+                String specialization1 = scan.next();
+                if (specialization1.toLowerCase().equals("mwa") || specialization1.toUpperCase().equals("MWA") && specialization1.toLowerCase().equals("aa") || specialization1.toUpperCase().equals("MAA") )
+                {
+                    System.out.println("VALID");
+                } else
+                    System.out.println("INVALID");
+    
+           }
+            
+            // If BSCS
+            else if (course.toLowerCase().equals("bscs") || course.toUpperCase().equals("BSCS")) {
+                System.out.print("Enter your specialization (DF or ML): ");
+                String specialization2 = scan.next();
+                if (specialization2.toLowerCase().equals("df") || specialization2.toUpperCase().equals("DF") && specialization2.toLowerCase().equals("ml") || specialization2.toUpperCase().equals("ML"))
+                {
+                    System.out.println("VALID");
+                } else
+                    System.out.println("INVALID");
+            }
         }
-
-        // If BSCS
-        else if (course.toLowerCase().equals("bscs") || course.toUpperCase().equals("BSCS")) {
-
-            System.out.print("Enter your specialization (DF or ML): ");
-            String specialization2 = scan.next();
-            if (specialization2.toLowerCase().equals("df") || specialization2.toUpperCase().equals("DF"))
-            {
-                System.out.println("YOUR SPEACILIZATION IS DF");
-            } else
-                System.out.println("YOUR SPEACILIZATION IS ML");
-        }
-
-    }
 
         
 
@@ -234,7 +232,7 @@ public class Exam {
         // HOME ECONOMICS
         // ARTS AND DESIGN
         // TVL MARITIME
-        static void strand(){ 
+        static void Q9(){ 
         System.out.println("9. SHS Strand selection ");
 
 
@@ -249,22 +247,22 @@ public class Exam {
         String strand = scan.next();
         switch(strand){
             case "STEM":
-            System.out.println("Your strand is STEM");
+            System.out.println("VALID");
             break;
             case "ICT":
-            System.out.println("Your strand is ICT");
+            System.out.println("VALID");
             break;
             case "HUMSS":
-            System.out.println("Your strand is HUMSS");
+            System.out.println("VALID");
             break;
             case "HOME ECONOMICS":
-            System.out.println("Your strand is HOME ECONOMICS");
+            System.out.println("VALID");
             break;
             case "ARTS AND DESIGN":
-            System.out.println("Your strand is ARTS AND DESIGN");
+            System.out.println("VALID");
             break;
             case "TVL MARITIME":
-            System.out.println("Your strand is TVL MARITIME");
+            System.out.println("VALID");
             break;
             default:
             System.out.println("Invalid");
@@ -280,14 +278,81 @@ public class Exam {
         // - The string "@student.national-u.edu.ph"
         //(10 pts)
 
-        static void email(){
-        System.out.println("10. Validate student email address ");
+        static void Q10(){
+            System.out.println("10. Validate student email address ");
+
         System.out.print("Enter your student email address: ");
+
         String studentEmailAddress = scan.next();
 
-        if (studentEmailAddress.contains("montales")) {
-            System.out.println("VALID EMAIL");
-        } else
-            System.out.println("INVALID EMAIL");
-    }
+
+
+        if (studentEmailAddress.contains("montales") && studentEmailAddress.contains("@student.national-u.edu.ph")) {
+
+            System.out.println("VALID");
+
+        } else {
+
+            System.out.println("INVALID");
+
         }
+    }
+
+  
+        // ==============================================================================
+        // Programming competition/Job interview level question (20 points)
+
+        // 11. Think like an ATM.
+
+        // You have an N amount of pesos in your savings account and you have decided
+        // that you want to withdraw all of your money in the ATM.
+
+        // The denomination of peso bills that the ATM could dispense are 20, 50, 100,
+        // 200, 500, 1000.
+
+        // Create a method that will return the minimum number of peso bills that the
+        // ATM will dispense after withdrawing all of your money.
+
+        // Example:
+        // Elizer has 150 pesos in his bank account. If elizer withdraws 150 pesos in
+        // the ATM, the ATM will dispense two peso bills (100 peso bill and 50 peso
+        // bill). (Answer is 2)
+
+        static void Q11(){
+        int tempFive = 0, tempTwoH = 0, tempOneH = 0, tempfifty = 0, temptwenty = 0;
+
+        System.out.println("11. Think like an ATM");
+        System.out.print("Enter the total amount of money in your bank account: ");
+        int amount = scan.nextInt();
+
+        int tempk = amount / 1000;
+        int tempMod = amount % 1000;
+        int tempamt = amount - 1000;
+        if (tempMod == 0 ) {
+            System.out.println ("(" + tempk + ") thousand");
+            
+        }    
+        else if (tempamt >= 500) {
+            tempFive = tempamt / 500;
+            tempamt = tempamt - 500;
+                 if (tempamt >= 200) {
+                    tempTwoH = tempamt / 200;
+                    tempamt = tempamt - 200;
+                } if (tempamt >= 100) {
+                    tempOneH = tempamt / 100;
+                    tempamt = tempamt - 100;
+                } if (tempamt >= 50) {
+                    tempfifty = tempamt / 50;
+                    tempamt = tempamt - 50;
+                } if (tempamt >= 20) {
+                    temptwenty = tempamt / 20;
+                    tempamt = tempamt - 20;
+                }
+        
+        System.out.println("(" + tempk + ") thoudsand (" + tempFive + ") 500 hundred (" + tempTwoH + ") 200 hundred (" + tempOneH + ") 100 hundred (" + tempfifty + ") 50 pesos (" + temptwenty + ") 20 pesos");
+        }
+        System.out.println("");
+        // Close scanner
+        scan.close();
+    }
+}
